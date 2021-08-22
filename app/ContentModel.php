f91a9a7f25edfb80ae00c715f4549d6a190107db
Daniel Brendel <dbrendel1988@gmail.com>
2021-08-22 11:31:50 +0200

    File query support

--- app/ContentModel.php
+++ app/ContentModel.php
@@ -7,11 +7,47 @@ use Illuminate\Support\Facades\Cache;
 
 class ContentModel extends Model
 {
+    /**
+     * @param string $field
+     * @param string $lang
+     * @return string
+     * @throws \Exception
+     */
     public static function queryContent(string $field, string $lang = 'en')
     {
         return Cache::remember($field . '_' . $lang, 120, function() use ($field, $lang) {
-            $item = ContentModel::where('lang', '=', $lang)->first();
-            return $item->$field;
+            if (env('APP_QUERYTYPE') === 'db') {
+               return static::queryContentDb($field, $lang);
+            } else if (env('APP_QUERYTYPE') === 'file') {
+                return static::queryContentFile($field, $lang);
+            } else {
+                throw new \Exception('Invalid query type specified: ' . env('APP_QUERYTYPE'));
+            }
         });
+    }
+
+    /**
+     * @param string $field
+     * @param string $lang
+     * @return string
+     */
+    private static function queryContentDb(string $field, string $lang)
+    {
+        $item = ContentModel::where('lang', '=', $lang)->first();
+        return $item->$field;
+    }
+
+    /**
+     * @param string $field
+     * @param string $lang
+     * @return string
+     */
+    private static function queryContentFile(string $field, string $lang)
+    {
+        if (!file_exists(base_path() . '/page_contents/' . $lang . '/' . $field . '.txt')) {
+            return null;
+        }
+
+        return file_get_contents(base_path() . '/page_contents/' . $lang . '/' . $field . '.txt');
     }
 }
