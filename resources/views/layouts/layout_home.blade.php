5ad0ec47e724715287727afb7087689b06978ba7
Daniel Brendel <dbrendel1988@gmail.com>
2023-01-18 00:45:51 +0100

    Mark online

--- resources/views/layouts/layout_home.blade.php
+++ resources/views/layouts/layout_home.blade.php
@@ -100,7 +100,7 @@
                                 geekflash.net
                             </a>
                             <a class="navbar-item" href="{{ url('/services/lachanfall') }}">
-                                lachanfall.net
+                                lachanfall.net &nbsp;<i class="fas fa-circle fa-sm is-online"></i>
                             </a>
                             <a class="navbar-item" href="{{ url('/services/astarlove') }}">
                                 astarlove.com
