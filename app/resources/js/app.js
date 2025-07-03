bf141501cf92d095c17ace678b29e413370cc4a1
Daniel Brendel <dbrendel1988@gmail.com>
2025-07-03 12:23:02 +0200

    Move scripts to suitable place

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -14,6 +14,119 @@ import 'highlight.js/scss/github.scss';
 
 window.hljs = hljs;
 
+window.ajaxRequest = function (method, url, data = {}, successfunc = function(data){}, finalfunc = function(){}, config = {})
+{
+    let func = window.axios.get;
+    if (method == 'post') {
+        func = window.axios.post;
+    } else if (method == 'patch') {
+        func = window.axios.patch;
+    } else if (method == 'delete') {
+        func = window.axios.delete;
+    }
+
+    func(url, data, config)
+        .then(function(response){
+            successfunc(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .finally(function(){
+                finalfunc();
+            }
+        );
+};
+
+window.switchProjectTab = function(which) {
+    for (let i = 1; i <= window.maxProjects; i++) {
+        let elHide = document.getElementById('tab-project-' + i);
+        if (elHide) {
+            if (!elHide.classList.contains('is-hidden')) {
+                elHide.classList.add('is-hidden');
+            }
+        }
+
+        let roleHide = document.getElementById('role-project-' + i);
+        if (roleHide) {
+            roleHide.ariaSelected = false;
+        }
+    }
+
+    let targetTab = document.getElementById('tab-project-' + which);
+    if (targetTab) {
+        if (targetTab.classList.contains('is-hidden')) {
+            targetTab.classList.remove('is-hidden');
+        }
+    }
+
+    let targetRole = document.getElementById('role-project-' + which);
+    if (targetRole) {
+        targetRole.ariaSelected = true;
+    }
+};
+
+window.toggleWindowSize = function(wnd, style) {
+    let elem = document.querySelector(wnd);
+    if (elem) {
+        elem.classList.toggle(style);
+    }
+};
+
+window.updateDateTime = function(target) {
+    let elem = document.querySelector(target);
+    if (elem) {
+        let dt = new Date();
+        elem.innerText = ('0' + dt.getHours()).slice(-2) + ':' + ('0' + dt.getMinutes()).slice(-2) + ':' + ('0' + dt.getSeconds()).slice(-2);
+
+        setTimeout(function() {
+            window.updateDateTime(target);
+        }, 1000);
+    }
+};
+
+window.fetchBlogPosts = function(target) {
+    let elem = document.querySelector(target);
+    if (elem) {
+        window.ajaxRequest('get', window.location.origin + '/blog/posts/fetch?limit=' + elem.dataset.limit, {}, function(response) {
+            if (response.code == 200) {
+                elem.innerHTML = '';
+
+                response.data.forEach(function(post, index) {
+                    let tableEntry = `
+                        <tr>
+                            <td><a href="` + window.location.origin + '/blog/' + post.slug + `">` + post.title + `</a></td>
+                            <td>` + post.created_at + `</td>
+                        </tr>
+                    `;
+
+                    elem.innerHTML += tableEntry;
+                });
+            }
+        });
+    }
+};
+
+window.queryShout = function(target) {
+    let container = document.querySelector(target);
+    if (container) {
+        window.ajaxRequest('get', window.location.origin + '/shoutbox/query', {}, function(response) {
+            if (response.code == 200) {
+                let tableEntry = `
+                    <tr>
+                        <td>` + response.shout.username + `</td>
+                        <td>` + response.shout.message + `</td>
+                    </tr>
+                `;
+
+                container.children[0].innerHTML += tableEntry;
+
+                container.scrollTop = container.scrollHeight 
+            }
+        });
+    }
+};
+
 window.previewBlogPost = function(title, content, token) {
     const form = document.createElement('form');
     form.method = 'POST';
