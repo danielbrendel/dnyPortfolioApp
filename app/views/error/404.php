d978dacc4c515c590baf05f4ec3535033aeb9771
Daniel Brendel <dbrendel1988@gmail.com>
2025-09-25 02:03:40 +0200

    Fix error 404 handler

--- app/views/error/404.php
+++ app/views/error/404.php
@@ -1,17 +1,19 @@
-<!-- Error 404 yield file -->
+<div class="column is-half" id="column-window-error-404">
+	<div class="window">
+		<div class="title-bar">
+			<div class="title-bar-text">Error 404</div>
+			<div class="title-bar-controls">
+				<button aria-label="Minimize" onclick="window.toggleWindowSize('#column-window-error-404', 'window-minimized');"></button>
+				<button aria-label="Maximize" onclick="window.toggleWindowSize('#column-window-error-404', 'window-maximized');"></button>
+				<button aria-label="Close"></button>
+			</div>
+		</div>
+		<div class="window-body">
+			<div class="is-centered">
+				<p>The requested resource <strong>{{ $_SERVER['REQUEST_URI'] }}</strong> was not found on the server.</p>
 
-<div class="outer">
-	<div class="inner">
-        <div class="title">
-            <h1>Error 404</h1>
-        </div>
-
-        <div class="text">
-            <p>The requested resource {{ $_SERVER['REQUEST_URI'] }} was not found on the server.</p>
-        </div>
-
-        <div class="links">
-            <button type="button" class="button btn-col-contact" onclick="location.href = '{{ url('/') }}';">Go home</button>
-        </div>
-    </div>
+				<a class="btn is-half is-pointer" href="{{ url('/') }}">Go home</a>
+			</div>
+		</div>
+	</div>
 </div>
